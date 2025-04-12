12589406610d581ee5d0c908e59a759059fdbc68
rasimul <rasimul29928@gmail.com>
2025-04-12 18:26:51 +0530

    fix issue and make responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
   title: "Md Rasimul Islam",
@@ -25,8 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`m-0`}
       >
+        <Toaster/>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 import { Contact } from "@/components/Contact";
 import Home from "@/components/Home";
-import { Toaster } from "react-hot-toast";
 export default function Page() {
   return (
-    <div className="">
-      <Toaster />
+    <div className="overflow-x-hidden">
       <Home />
       <Contact />
     </div>
